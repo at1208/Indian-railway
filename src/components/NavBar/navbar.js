39ae4bae9d5555439c7b5ab9838d849ac077fe7c
aman tiwari <amantiwari12081999@gmail.com>
2019-07-10 20:46:08 +0530

    some styling improved

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -3,35 +3,34 @@ import './navbar.css'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-  return <div className='container'>
+  return <div className='container text-center'>
     <div className='row'>
-      <button className='col-sm btn btn-sm shadow text-center a1'>
-      <Link to='live-train-status' className='a'> Live Train status </Link>
-      </button>
 
-      <button className='col-sm btn btn-sm shadow text-center a1'>
-      <Link to='/pnr-status' className='a'>PNR status</Link>
-      </button>
+    <Link to='live-train-status' className='col-lg  shadow text-center a1'>
+       Live Train status
+    </Link>
 
-
-
-      <button className='col-sm btn btn-sm shadow text-center a1'>
-      <Link to='/seat-availability' className='a'>Seat Availability</Link>
-      </button>
-
-      <button className='col-sm btn btn-sm shadow text-center a1'>
-      <Link to='/train-between-stations' className='a'> Train Between Stations </Link>
-      </button>
-
-      <button className='col-sm  btn btn-sm shadow text-center a1'>
-      <Link to='/train-fair' className='a'>Train Fair Enquiry</Link>
-      </button>
+      <Link to='/pnr-status' className='col-lg shadow text-center a1 '>
+      PNR status
+      </Link>
 
 
+      <Link to='/seat-availability' className='col-lg shadow text-center a1 '>
+      Seat Availability
+      </Link>
 
-      <button className='col-sm btn btn-sm shadow text-center a1'>
-      <Link to='/cancelled-train' className='a'>Cancelled Train</Link>
-      </button>
+      <Link to='/train-between-stations' className='col-lg  shadow text-center a1 '>
+       Train B/W Stations
+      </Link>
+
+       <Link to='/train-fair' className=' col-lg shadow text-center a1 '>
+      Train Fair Enquiry
+      </Link>
+
+
+      <Link to='/cancelled-train' className='col-lg shadow text-center a1 '>
+      Cancelled Train
+      </Link>
 
     </div>
   </div>
